848f7b116083d94f72801a6cc23ef0ea1ecbd6c0
JasmeetsMacDesktop <jswasal@gmail.com>
2020-09-15 15:15:13 +0530

    Updates

---
+++ precache-manifest.5eca75c155f3ef073c0a84565ebae29b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0857f48d6fc55ef7f5fe45466b059f13",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "9a95c90a3db3e6cae12c",
+    "url": "/MyPortfolio/static/css/main.8125f7e6.chunk.css"
+  },
+  {
+    "revision": "06b5210f74eaa1d76e47",
+    "url": "/MyPortfolio/static/js/2.1f84496e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyPortfolio/static/js/2.1f84496e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a95c90a3db3e6cae12c",
+    "url": "/MyPortfolio/static/js/main.293e01cb.chunk.js"
+  },
+  {
+    "revision": "545e9cc0ba06ec497362",
+    "url": "/MyPortfolio/static/js/runtime-main.7f417827.js"
+  },
+  {
+    "revision": "719f3c836fa38f6ef336953f903131f1",
+    "url": "/MyPortfolio/static/media/Header.719f3c83.jpg"
+  },
+  {
+    "revision": "bc65520ebe1b682adeb4ebe9f730e37a",
+    "url": "/MyPortfolio/static/media/PortfolioPic.bc65520e.png"
+  }
+]);
